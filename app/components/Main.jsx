cf1fc58c5daa39108d62951a2ef98b30fc0fc89d
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-11 17:36:25 +1300

    Navigation component showing on the browser

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,11 +1,12 @@
 var React = require('react')
-
+const Navigation = require('Navigation')
 
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
+          <Navigation/>
           <p>Main.jsx Render</p>
           {props.children}
         </div>
