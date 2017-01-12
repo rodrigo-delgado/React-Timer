00ebc33b9449f1ba76a4c0d5e21b2ff86af35a72
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-13 08:13:01 +1300

    CountdownForm passes the test

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -1,13 +1,35 @@
-var expect = require('expect')
-var React= require('react')
-var ReactDOM = require('react-dom')
-var TestUtils = require('react-addons-test-utils')
-var $ = require('jQuery')
+const expect = require('expect')
+const React= require('react')
+const ReactDOM = require('react-dom')
+const TestUtils = require('react-addons-test-utils')
+const $ = require('jQuery')
 
-var CountdownForm = require('CountdownForm')
+const CountdownForm = require('CountdownForm')
 
 describe('CountdownForm', () => {
   it('should exist', () => {
     expect(CountdownForm).toExist()
+  })
+  //testing GOOD data
+  it('should call onSetCountdown if valid seconds entered', () => {
+    const spy = expect.createSpy()
+    const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>)
+    const $el = $(ReactDOM.findDOMNode(countdownForm))
+
+    countdownForm.refs.seconds.value = '109'
+    TestUtils.Simulate.submit($el.find('form')[0])
+
+    expect(spy).toHaveBeenCalledWith(109)
+  })
+  //testing BAD data
+  it('should not call onSetCountdown if invalid seconds entered', () => {
+    const spy = expect.createSpy()
+    const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>)
+    const $el = $(ReactDOM.findDOMNode(countdownForm))
+
+    countdownForm.refs.seconds.value = 'aq'
+    TestUtils.Simulate.submit($el.find('form')[0])
+
+    expect(spy).toNotHaveBeenCalled(109)
   })
 })
